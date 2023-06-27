c7951c8a1d03ebe974b9b4ce348754c48be80fa5
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2023-06-27 11:26:04 -0400

    Create generic passport parser

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Azure;
+using Azure.AI.FormRecognizer;
+using Azure.AI.FormRecognizer.Models;
+
+namespace PassportParser
+{
+    class Program
+    {
+        private const string endpoint = "<your-form-recognizer-endpoint>";
+        private const string apiKey = "<your-form-recognizer-api-key>";
+
+        private static void Main(string[] args)
+        {
+            var passportFilePath = args[0]; //Assume the first argument is the passport image file path
+
+            var client = AuthenticateClient(endpoint, apiKey);
+            RecognizePassportAsync(client, passportFilePath).Wait();
+        }
+
+        static FormRecognizerClient AuthenticateClient(string endpoint, string apiKey)
+        {
+            var credential = new AzureKeyCredential(apiKey);
+            var client = new FormRecognizerClient(new Uri(endpoint), credential);
+            return client;
+        }
+
+        static async Task RecognizePassportAsync(FormRecognizerClient client, string passportFilePath)
+        {
+            using var stream = new FileStream(passportFilePath, FileMode.Open);
+            var options = new RecognizeContentOptions();
+            var operation = await client.StartRecognizeContentAsync(stream, options);
+            var operationResponse = await operation.WaitForCompletionAsync();
+            var forms = operationResponse.Value;
+
+            //Print recognized details in JSON format
+            Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(forms));
+        }
+    }
+}
